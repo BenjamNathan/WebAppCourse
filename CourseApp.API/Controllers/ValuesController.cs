b4e686215274b3ba0299c2fdf6c697f2ade271be
Ben Nathan <Ben.Nathan@game.co.uk>
2018-09-25 16:08:02 +0100

    Created solution for dotnet and angular. Created database with access to it

--- CourseApp.API/Controllers/ValuesController.cs
+++ CourseApp.API/Controllers/ValuesController.cs
@@ -2,26 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseApp.API.Controllers
 {
+    // http://localhost:5000/api/values  This URL would hit the first method (GET api/values) Assuming this is a GET URL
+    // If the URL ended /values/5 this would hit the second method and that's what the browser would return
+    // App knows to point to localhost:5000 because it's set in the Properties > launchSettings.json file
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        // IActionResult allows us to return http responses to the client       
+        // Made asynchronous because it keeps the connection open allowing multiple request to be performed at the same time
+        // If it wasn't then each request would have to wait for the other to finish
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+
+            // await used to make it an async method and ToListAsync to 
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
