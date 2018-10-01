c945689a9e71f586518f82edea684adca5c15333
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-01 11:03:34 +0100

    Created helper to add headers into error responses

--- CourseApp.API/Controllers/AuthController.cs
+++ CourseApp.API/Controllers/AuthController.cs
@@ -46,6 +46,8 @@ namespace CourseApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("It gone done errored!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
