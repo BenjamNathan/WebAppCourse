2278f7f4b1b32b287c61abcb165d881643f6c47f
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-01 12:13:20 +0100

    Implemented error interceptors to handle errors on the client side

--- CourseApp.API/Controllers/AuthController.cs
+++ CourseApp.API/Controllers/AuthController.cs
@@ -46,8 +46,6 @@ namespace CourseApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("It gone done errored!");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
