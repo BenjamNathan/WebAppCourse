42d90fa41c630863f6a1a5d98574aa5812286efa
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-11 12:55:05 +0100

    User can register via UI and be auto logged in

--- CourseApp-SPA/src/app/register/register.component.ts
+++ CourseApp-SPA/src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -17,13 +19,13 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   // Output properties emit events so need to create a new EventEmitter
 
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
   // The BsDatepickerConfig class has a lot of required fields but we only wanted to implemented the containerClass
   // in order to customise the theme. Setting it to a partial class makes all required fields optional
 
-  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) {}
+  constructor(private authService: AuthService, private router: Router, private alertify: AlertifyService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.bsConfig = {
@@ -61,14 +63,28 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      // this.registerForm.value takes all the values from the form and clones them into the empty object.
+      // The object is then assigned to user
+      this.authService.register(this.user).subscribe( () => {
+        this.alertify.success('Registration Successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe( () => {
+          this.router.navigate(['/available-shooters']);
+        });
+      });
+    }
 
+    // Original register method
     // this.authService.register(this.model).subscribe(() => {
     //   this.alertify.success('Registration Successful');
     //   // Using emptpy brackets because after this is successful no information is needed from the respoonse
     // }, error => {
     //   this.alertify.error(error);
     // });
-    console.log(this.registerForm.value);
   }
 
   cancel() {
