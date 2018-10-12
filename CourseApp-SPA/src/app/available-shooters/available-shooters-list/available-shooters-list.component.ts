ebc64ab147c0a9e299d896a4850a534e0cb50c37
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-12 10:45:13 +0100

    Correct users load on pagination

--- CourseApp-SPA/src/app/available-shooters/available-shooters-list/available-shooters-list.component.ts
+++ CourseApp-SPA/src/app/available-shooters/available-shooters-list/available-shooters-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-available-shooters-list',
@@ -11,14 +12,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AvailableShootersListComponent implements OnInit {
   users: User[];
-  id: number;
+  pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  // different loadUsers method for loading on pagination
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe((res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+      }, error => {
+        this.alertify.error(error);
+      });
   }
 
   /*
