2e6c90910b0393f1cd3c69d5bd87cfc773ce09a2
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-15 10:01:01 +0100

    Able to filter users on likers and likees list

--- CourseApp.API/Helpers/UserParams.cs
+++ CourseApp.API/Helpers/UserParams.cs
@@ -22,5 +22,7 @@ namespace CourseApp.API.Helpers
         public string ShooterType { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99;
+        public bool Likees { get; set; } = false;
+        public bool Likers { get; set; } = false;
     }
 }
