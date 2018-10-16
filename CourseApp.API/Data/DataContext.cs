7473f2d4f863d69c8ce6aa8621bb0455e2784fe9
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-16 15:36:35 +0100

    Updating Data Context for Identity

--- CourseApp.API/Data/DataContext.cs
+++ CourseApp.API/Data/DataContext.cs
@@ -1,22 +1,46 @@
 using CourseApp.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CourseApp.API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User, Role, int,
+        IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>  //Used to just inherit from DbContext 
+        //Only added in *<int> because the default is of type string so need to mention them to use int
     {
-        public DataContext(DbContextOptions<DataContext> options) : base (options)
-        {            
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
         }
 
         public DbSet<Value> Values { get; set; }
-        public DbSet<User> Users { get; set; }
+        // No longer need this because it comes from asp .net core Identity
+        // public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            // This configures the schema for Identity framework
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+            });
+
             // Because Like class has 2 id fields, we need to tell Entity Framwork what we want
             // as a primary key (the id field in the database) and to make it unique it's going to be a combination of both
             builder.Entity<Like>()
