2ada7b6eadfbd9ed7da8459a974f95e66e8f4c90
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-16 15:05:53 +0100

    Created classes to start journey towards using Identity

--- CourseApp.API/Models/User.cs
+++ CourseApp.API/Models/User.cs
@@ -1,14 +1,16 @@
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 
 namespace CourseApp.API.Models
 {
-    public class User
+    public class User : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        // Refactoring code to use Identity and these properties already exist as part of the IdentityUser class
+        // public int Id { get; set; }
+        // public string Username { get; set; }
+        // public byte[] PasswordHash { get; set; }
+        // public byte[] PasswordSalt { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
@@ -25,5 +27,6 @@ namespace CourseApp.API.Models
         public ICollection<Like> Likees { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
     }
 }
