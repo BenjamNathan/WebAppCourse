fde462c988997176f1535825c96231307c71dc3f
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-17 14:28:31 +0100

    Able to add and remove user roles via the API

--- CourseApp.API/Controllers/AdminController.cs
+++ CourseApp.API/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 ﻿using CourseApp.API.Data;
+using CourseApp.API.Dtos;
+using CourseApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,15 +17,17 @@ namespace CourseApp.API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public AdminController(DataContext context)
+        public AdminController(DataContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // Authorize policy matches name in start up class
         [Authorize(Policy = "RequireAdminRole")]
-        [HttpGet("userWithRoles")]        
+        [HttpGet("usersWithRoles")]        
         public async Task<IActionResult> GetUsersWithRoles()
         {
             var userList = await (from user in _context.Users orderby user.UserName
@@ -38,6 +43,34 @@ namespace CourseApp.API.Controllers
                                   }).ToListAsync();
 
             return Ok(userList);
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("editRoles/{userName}")]
+        public async Task<IActionResult> EditRoles(string userName, RoleEditDto roleEditDto)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var selectedRoles = roleEditDto.RoleNames;
+
+            selectedRoles = selectedRoles ?? new string[] {};
+            // ?? is the null coalescing operator which expands to:
+            // selectedRoles = selectedRoles != null ? selectedRoles : new string[] {}
+            // Essentially means, if not null use what's on the left, if it is use what's on the right
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed to remove the roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
