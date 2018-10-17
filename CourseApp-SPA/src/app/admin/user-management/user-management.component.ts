45e65235d5c229ee36e810d91a4e286802bbc037
Ben Nathan <Ben.Nathan@game.co.uk>
2018-10-17 21:43:40 +0100

    Hooked editing user roles on the front end with the API

--- CourseApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ CourseApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -13,18 +13,21 @@ export class UserManagementComponent implements OnInit {
   users: User[];
   bsModalRef: BsModalRef;
 
-  constructor(private adminService: AdminService, private modalService: BsModalService) { }
+  constructor(private adminService: AdminService, private modalService: BsModalService) {}
 
   ngOnInit() {
     this.getUsersWithRoles();
   }
 
   getUsersWithRoles() {
-    this.adminService.getUsersWithRoles().subscribe((users: User[]) => {
-      this.users = users;
-    }, error => {
-      console.log(error);
-    });
+    this.adminService.getUsersWithRoles().subscribe(
+      (users: User[]) => {
+        this.users = users;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
   editRolesModal(user: User) {
@@ -32,11 +35,21 @@ export class UserManagementComponent implements OnInit {
       user,
       roles: this.getRolesArray(user)
     };
-    this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
-    this.bsModalRef.content.updateSelectedRoles.subscribe((values) => {
+    this.bsModalRef = this.modalService.show(RolesModalComponent, { initialState });
+    this.bsModalRef.content.updateSelectedRoles.subscribe(values => {
       const rolesToUpdate = {
         roleNames: [...values.filter(el => el.checked === true).map(el => el.name)]
       };
+      if (rolesToUpdate) {
+        this.adminService.updateUserRoles(user, rolesToUpdate).subscribe(
+          () => {
+            user.roles = [...rolesToUpdate.roleNames];
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
     });
   }
 
@@ -44,10 +57,10 @@ export class UserManagementComponent implements OnInit {
     const roles = [];
     const userRoles = user.roles;
     const availableRoles: any[] = [
-      {name: 'Admin', value: 'Admin'},
-      {name: 'Moderator', value: 'Moderator'},
-      {name: 'Member', value: 'Member'},
-      {name: 'VIP', value: 'VIP'}
+      { name: 'Admin', value: 'Admin' },
+      { name: 'Moderator', value: 'Moderator' },
+      { name: 'Member', value: 'Member' },
+      { name: 'VIP', value: 'VIP' }
     ];
 
     for (let i = 0; i < availableRoles.length; i++) {
@@ -67,5 +80,4 @@ export class UserManagementComponent implements OnInit {
     }
     return roles;
   }
-
 }
